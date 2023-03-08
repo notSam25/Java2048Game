b20660128cdf223f6ab9f4a9beac6c8d9725d92a
Samuel Wiseman <email@gmail.com>
2023-03-08 14:00:53 -0600

    add maven

---
+++ Java2048/build.sh
@@ -0,0 +1,1 @@
+mvn compile package && java -cp target/Java2048-1.0-SNAPSHOT.jar com.Java2048.app.App
---
+++ Java2048/src/main/java/com/Java2048/app/App.java
@@ -0,0 +1,11 @@
+package com.Java2048.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
